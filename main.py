3fd5274b7790212b652aaa86252dc1dfea3ccce4
Sławek Maciejewski <slawek74.m@interia.pl>
2020-12-28 16:25:23 +0000

    Refactored a function 'generate_random_data ()' it returns a list of dict. Added a new function 'compare_answer(answer, data_A, data_B)' which takes three attributes and returns True or False. The main while loop has been rebuilt.

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ from game_data import data
 # generate random data
 def generate_random_data():
     """
-    Function generate one random data from data list. Return dict with keys:
+    Function generate random data from data list. Return list of dicts with keys:
     'name'-> str, 'follower_count'-> int, 'description'-> str, 'country-> str'
     """
-    random_data = random.choice(data)
-    return random_data
+    random_data_A = random.choice(data)
+    random_data_B = random.choice(data)    
+    while random_data_A['name'] == random_data_B['name']:
+        print("the same data")
+        random_data_B = random.choice(data)
+    list_of_data = [random_data_A, random_data_B]
+    return list_of_data
 
-
+# Display formatted questions
 def formatted_text(data_A, data_B):
     """
     Function print formatted questions from random data and pass it to compare them. 
@@ -27,52 +32,39 @@ def formatted_text(data_A, data_B):
         f"Agains B: {data_B['name']}, a {data_B['description']}, from {data_B['country']}."
     )
 
+# compare answer
+def compare_answer(answer, data_A, data_B):
+    """ 
+    The function takes three attributes 'answer' -> str, 'data_A' -> dict, 'data_B' -> dict.
+    Function return True or False. 
+    """
+    if answer == 'a' and data_A['follower_count'] > data_B['follower_count']:        
+        return True
+    elif answer == 'b' and data_B['follower_count'] > data_A['follower_count']:          
+        return True
+    else:
+        return False
 
-# def compare_answer(answer, data_A, data_B):
-#     score = 0
-#     if answer == 'a' and data_A['follower_count'] > data_B['follower_count']:
-#         score += 1
-#         data_A = data_B
-#         while data_A['name'] == data_B['name']:
-#             print("the same data")
-#             data_B = generate_random_data()
-#     elif answer == 'b' and data_B['follower_count'] > data_A['follower_count']:
-#         score += 1
-#         data_A = data_B
-#     else:
-#         is_play = False
-#         return is_play
 
 
 score = 0
 is_play = True
-data_A = generate_random_data()
-data_B = generate_random_data()
-while data_A['name'] == data_B['name']:
-    print("the same data")
-    data_B = generate_random_data()
-# print(data_A)
-# print(data_B)
+questions = generate_random_data()
+data_A = questions[0]
+data_B = questions[1]
+
 while is_play:
     clear()
     print(logo)
     print(f"Your score is {score}")
     formatted_text(data_A, data_B)
     answer = input("Who has more followers, type 'A' or 'B': ").lower()
-    #compare_answer(answer, data_A, data_B)
-    if answer == 'a' and data_A['follower_count'] > data_B['follower_count']:
+    if compare_answer(answer, data_A, data_B):
         score += 1
         data_A = data_B
-        data_B = generate_random_data()
         while data_A['name'] == data_B['name']:
             print("the same data")
-            data_B = generate_random_data()
-    elif answer == 'b' and data_B['follower_count'] > data_A['follower_count']:
-        score += 1
-        data_A = data_B
-        data_B = generate_random_data()
-        while data_A['name'] == data_B['name']:
-            print("the same data")
-            data_B = generate_random_data()
+            data_B = generate_random_data()[0]
     else:
         is_play = False
+    
